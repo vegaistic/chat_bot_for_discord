6ae0a2b73ffea60b091f78d9c68e25a2f17571bc
Jose Vega <josevega3923@yahoo.com>
2019-07-10 23:28:34 -0400

    jose

--- Chatbotdiscord (1).py
+++ Chatbotdiscord (1).py
@@ -1,19 +1,34 @@
 import random
+import discord
+import logging
+
+logging.basicConfig(level = logging.INFO)
+
+
+
+class my_client(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+        
 
 class Chat_bot():
 
-    def __init__(self, jokes, greetings, avatar):
+    def __init__(self, jokes, greetings, avatar, name):
         self.jokes = jokes
         self.greetings = greetings
         self.avatar = avatar
+        self. name = name
 
     def greeting(self):
         global ace_greetings
-        print(random.choice(self.greetings))
-        choice = input("\nwanna hear some jokes? ")
-        if choice == 'yes':
+        print(self.name,":", random.choice(self.greetings))
+        choice = input("\n{}: wanna hear some jokes? ".format(self.name))
+        if 'y' in choice:
             self.tell_joke()
-        elif choice == "NO":
+        elif choice == "no":
             print('see ya later!')
             exit()
         else:
@@ -22,8 +37,8 @@ class Chat_bot():
     def tell_joke(self):
         global ace_jokes
         print(random.choice(self.jokes))
-        choice = input("\nWanna hear another joke?")
-        if choice == 'yes':
+        choice = input("\n{}: Wanna hear another joke? ".format(self.name))
+        if 'y' in choice:
             self.tell_joke()
         elif choice =='no':
             print('see ya later baby!')
@@ -43,7 +58,7 @@ for jokes in ace_jokes_file:
 for greeting in ace_greetings_file:
     ace_greetings.append(greeting)
 
-ace_visconti = Chat_bot(ace_jokes, ace_greetings, False )
+ace_visconti = Chat_bot(ace_jokes, ace_greetings, False, "Ace Visconti" )
 
 
 ace_visconti.greeting()
