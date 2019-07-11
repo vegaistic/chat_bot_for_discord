2e2087a56b7c2a28c952ac887cfe213edec16b2a
Jose Vega <josevega3923@yahoo.com>
2019-07-11 10:54:06 -0400

    this is a modified whatever

--- Chatbotdiscord (1).py
+++ Chatbotdiscord (1).py
@@ -2,18 +2,6 @@ import random
 import discord
 import logging
 
-logging.basicConfig(level = logging.INFO)
-
-
-
-class my_client(discord.Client):
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
-
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
-        
-
 class Chat_bot():
 
     def __init__(self, jokes, greetings, avatar, name):
@@ -51,6 +39,11 @@ ace_greetings = []
 ace_greetings_file = open("ace_greetings.txt", "r")
 ace_jokes = []
 ace_jokes_file = open("ace_jokes.txt","r")
+ace_wisdom = []
+ace_wisdom_file = open("ace_wisdom.txt","r")
+
+for wisdom in ace_wisdom_file:
+    ace_wisdom.append(wisdom)
 
 for jokes in ace_jokes_file:
     ace_jokes.append(jokes)
@@ -61,7 +54,29 @@ for greeting in ace_greetings_file:
 ace_visconti = Chat_bot(ace_jokes, ace_greetings, False, "Ace Visconti" )
 
 
-ace_visconti.greeting()
+logging.basicConfig(level = logging.INFO)
+
+class my_client(discord.Client):
+
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        if message.author == client.user:
+            return
+        print('Message from {0.author}: {0.content}'.format(message))
+
+        if message.content.startswith('joke'):
+            await message.channel.send("Ace Visconti Says: {}".format(random.choice(ace_jokes)))
+        if message.content.startswith('hello'):
+            await message.channel.send(random.choice(ace_greetings))
+        if message.content.startswith('wisdom'):
+            await message.channel.send("Like Ace Visconti Always Says: {}".format(random.choice(ace_wisdom)))
+
+
+client = my_client()
+client.run('NTk4NzExMzkzNTAzNTQzMzE2.XSau0A.8wYkU4Ozrii0b48TnWJmZA1Dt7o')
+
 
 
 
